0ac096c14d054bd891b9e95abaa72e39e8ba24f7
tarnag57 <toth.viktor7400@gmail.com>
2018-09-09 12:31:17 +0100

    Rethinks input dimensions

--- tools/network.py
+++ tools/network.py
@@ -4,14 +4,28 @@ import numpy as np
 # Define a convolutional layer
 class Conv_layer(object):
 
-	def __init__(self, prev_sizes, after_sizes, filter_size):
+	# prev_sizes: W0*H0*D0
+	# filter_sizes: WF*HF*D0*DF
+	# biases: 1*1*1*DF => represented as 1D array
+	# output: W0*H0*DF
+	def __init__(self, prev_sizes, filter_size):
 		self.prev_sizes = prev_sizes
-		self.after_sizes = after_sizes
+		self.filter_sizes = filter_size
+
+		# Check input params
+		if filter_size[2] != prev_sizes[2]:
+			raise Exception('The filters should have the same depths as the input. Filter depth: ' 
+				+ filter_size[2] + ', input depth: ' + prev_sizes[2] )
+
 		self.filters = [np.random.randn(filter_size[0], filter_size[1], filter_size[2])
-								for y in after_sizes[0][1]]
+								for y in filter_size[3]]
+		self.biases = [np.random.randn(1) for y in filter_size[3]]
 
-	def feedforward(input):
+	def feedforward(self, input):
+		if input.shape != (self.prev_sizes[0], self.prev_sizes[1], self.prev_sizes[2]):
+			raise Exception('The input matrix has different sizes than expected. Input shape: ' + input.shape)
 
+		return  
 
 
 class Network(object):
